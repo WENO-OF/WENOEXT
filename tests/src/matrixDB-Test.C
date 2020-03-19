76aff2a9828bda8da23abe52f84583bd19b43ae7
Jan Gärtner <jan-wilhelm.gaertner@itv.uni-stuttgart.de>
2020-03-19 15:14:23 +0100

    Add test case for matrixDB

---
+++ tests/src/matrixDB-Test.C
@@ -0,0 +1,137 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    matrixDB-Test
+    
+Description
+    Test case for matrixDB
+    
+Author
+    Jan Wilhelm Gärtner <jan.gaertner@outlook.de>
+
+\*---------------------------------------------------------------------------*/
+
+#include "catch.hpp"
+
+#include "fvCFD.H"
+#include "matrixDB.H"
+#include "OFstream.H"
+#include "IFstream.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+TEST_CASE("matrixDB Test Case","[3D]")
+{
+    // Replace setRootCase.H for Catch2   
+    int argc = 1;
+    char **argv = static_cast<char**>(malloc(sizeof(char*)));
+    char executable[] = {'m','a','i','n'};
+    argv[0] = executable;
+    Foam::argList args(argc, argv,false,false,false);
+        
+    // create the mesh from case file
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    
+    
+    // Function to create matrix
+    auto createMatrix = [](const label cellI, const label stencilI) -> scalarRectangularMatrix
+    {
+        scalarRectangularMatrix A
+            (
+                5,
+                10,
+                scalar((cellI*stencilI) % 1000)
+            );
+        return A;
+    };
+    
+    // Matrix Data Bank
+    matrixDB matrixDataBank;
+    
+    // Create a list of matrices
+    List<List<scalarRectangularMatrix> > LSmatrix;
+    LSmatrix.resize(1000);
+    matrixDataBank.resize(LSmatrix.size());
+    
+    forAll(LSmatrix, cellI)
+    {
+        LSmatrix[cellI].resize(10);
+        matrixDataBank.resizeSubList(cellI,LSmatrix[cellI].size());
+        forAll(LSmatrix[cellI],stencilI)
+        {
+            LSmatrix[cellI][stencilI] = createMatrix(cellI,stencilI);
+            matrixDataBank[cellI][stencilI].add(createMatrix(cellI,stencilI));
+        }
+    }
+
+    // function to check the sum 
+    auto compareMatrix = [](const scalarRectangularMatrix A, const scalarRectangularMatrix B) -> void
+    {
+        for (int i = 0; i < A.m(); i++)
+        {
+            for (int j = 0; j < A.n(); j++)
+            {
+                REQUIRE(A[i][j] == B[i][j]);
+            }
+        }
+    };
+
+
+    // Display Info 
+    matrixDataBank.info();
+    
+    // Check that all matrix values are correct
+    
+    forAll(LSmatrix,cellI)
+    {
+        forAll(LSmatrix[cellI],stencilI)
+        {
+            compareMatrix(LSmatrix[cellI][stencilI],matrixDataBank[cellI][stencilI]());
+        }
+    }
+
+    fileName path = mesh.time().path()/"constant/matrixDataBankTest";
+    
+    OFstream osLS(path,OFstream::streamFormat::BINARY);
+    matrixDataBank.write(osLS);
+    
+    matrixDB newMatrixDB;
+    IFstream isLS(path,IFstream::streamFormat::BINARY);
+    newMatrixDB.read(isLS);
+    
+    // Check that all values are read in correctly:
+    forAll(LSmatrix,cellI)
+    {
+        forAll(LSmatrix[cellI],stencilI)
+        {
+            compareMatrix(LSmatrix[cellI][stencilI],newMatrixDB[cellI][stencilI]());
+        }
+    }
+
+
+    
+    
+}
