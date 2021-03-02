02874821f7844c338bd4dfc7b507fdf655ad143a
Jan Gärtner <jan-wilhelm.gaertner@itv.uni-stuttgart.de>
2021-03-02 14:24:16 +0100

    [BugFix] genCodeRules had incorrect paths

--- versionRules/genCodeRules.sh
+++ versionRules/genCodeRules.sh
@@ -26,20 +26,20 @@ fi
 versionFile=foamVersion4weno.H
 
 echo "here $(pwd)"
-${HOME_DIR}/versionRules/makeFoamVersionHeader.py $WM_PROJECT_VERSION >$versionFile.tmp
+${HOME_DIR}/versionRules/makeFoamVersionHeader.py $WM_PROJECT_VERSION > ${HOME_DIR}/versionRules/$versionFile.tmp
 
 
-if [ -e $versionFile ]; then                                                    
-    nrDiff=$(diff $versionFile.tmp $versionFile | wc -l | tr -d " ")            
+if [ -e ${HOME_DIR}/versionRules/$versionFile ]; then                                                    
+    nrDiff=$(diff ${HOME_DIR}/versionRules/$versionFile.tmp ${HOME_DIR}/versionRules/$versionFile | wc -l | tr -d " ")            
     if [[ $nrDiff > 0 ]]; then                                                  
     echo "$versionFile changed"                                                 
-    mv $versionFile.tmp $versionFile                                            
+    mv ${HOME_DIR}/versionRules/$versionFile.tmp ${HOME_DIR}/versionRules/$versionFile                                           
     else                                                                        
     # Make sure that not a complete recompilation is triggered                  
     echo "No change to $versionFile"                                            
-    rm $versionFile.tmp                                                         
+    rm ${HOME_DIR}/versionRules/$versionFile.tmp                                                         
     fi                                                                          
 else                                                                            
     echo "No $versionFile. Generating!"                                           
-    mv $versionFile.tmp $versionFile                                            
+    mv ${HOME_DIR}/versionRules/$versionFile.tmp ${HOME_DIR}/versionRules/$versionFile                                            
 fi                                          
