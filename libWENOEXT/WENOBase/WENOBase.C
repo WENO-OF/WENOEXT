80903cf18dc5e3eac0e8e383ecb75f9f7de5ea70
Jan Gärtner <jan-wilhelm.gaertner@itv.uni-stuttgart.de>
2021-08-27 16:13:16 +0200

    Init receiveHaloSize list explicitly
    The setSize default initializer is not working correctly for all gcc versions

--- libWENOEXT/WENOBase/WENOBase.C
+++ libWENOEXT/WENOBase/WENOBase.C
@@ -374,8 +374,12 @@ void Foam::WENOBase::distributeStencils
     #endif
 
     // Set size of receiveHaloSize list
-    receiveHaloSize_.setSize(Pstream::nProcs(),0);
-    
+    receiveHaloSize_.setSize(Pstream::nProcs());
+    forAll(receiveHaloSize_,procI)
+    {
+        receiveHaloSize_[procI] = 0;
+    }
+     
     // Send halo cell list to neighbouring processors to request cells
     // Store the size of receiving halos
     forAll(receiveProcList_, procI)
